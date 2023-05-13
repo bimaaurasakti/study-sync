33c44780dc94313805a4cec69d07be5c613384b3
Arahma <bimaaurasakti2001@gmail.com>
2023-05-13 20:49:18 +0700

    add login, register, and logout on navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,61 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row">
+        <div class="col">
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header fw-bold text-center">New</div>
+            </div>
 
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                  <h5 class="card-title">Dark card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
 
-                    {{ __('You are logged in!') }}
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Dark card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header fw-bold text-center">Inprogress</div>
+            </div>
+
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Dark card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Dark card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-header fw-bold text-center">Done</div>
+            </div>
+
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Dark card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+
+            <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">Dark card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,96 +14,68 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">
-                <h3 class="navbar-logo align-midle fw-bold mb-0">
-                    <img src="{{ asset('icons/rocket.svg') }}" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-                    <span class="l">Study</span><span class="r">Sync</span>
-                </h3>
-              </a>
-              {{-- <img src="{{ asset('images/profile.jpg') }}" class="rounded-circle" height="75"> --}}
-              {{-- <img src="{{ asset('images/profile.jpg') }}" class="img-fluid rounded-circle" height="25"> --}}
+                <a class="navbar-brand me-auto" href="#">
+                    <h3 class="navbar-logo align-middle fw-bold mb-0">
+                        <img class="align-middle me-1" src="{{ asset('icons/rocket.svg') }}" alt="Logo" width="16" class="d-inline-block align-text-top">
+                        <span class="l">Study</span><span class="r">Sync</span>
+                    </h3>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                @if (auth()->guest())
+                    <a class="nav-link me-3" href="{{ route('login') }}">
+                        Login
+                    </a>
+                    <a class="nav-link" href="{{ route('register') }}">
+                        Register
+                    </a>
+                @else
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item dropdown">
+                                <button type="button" class="btn profile rouded-circle p-0" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <div class="circular--portrait">
+                                        <img src="{{ asset('images/profile.jpg') }}" />
+                                    </div>
+                                </button>
+                                <div class="dropdown-menu dropdown-menu-end mt-3 p-1">
+                                    <a class="nav-link text-danger" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+                                    
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                @endif
             </div>
-          </nav>
+        </nav>
 
         <main class="py-4">
-            {{-- @yield('content') --}}
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-header text-center">New</div>
-                        </div>
-
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-body">
-                              <h5 class="card-title">Dark card title</h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-body">
-                              <h5 class="card-title">Dark card title</h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-header text-center">Inprogress</div>
-                        </div>
-
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-body">
-                              <h5 class="card-title">Dark card title</h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-body">
-                              <h5 class="card-title">Dark card title</h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-header text-center">Done</div>
-                        </div>
-
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-body">
-                              <h5 class="card-title">Dark card title</h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-
-                        <div class="card text-bg-dark mb-3" style="max-width: 18rem;">
-                            <div class="card-body">
-                              <h5 class="card-title">Dark card title</h5>
-                              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @yield('content')
         </main>
     </div>
 
     <!-- Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
 </body>
 </html>
