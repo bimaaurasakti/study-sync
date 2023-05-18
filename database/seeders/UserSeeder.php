49f5e50f328a40b735ca9ae34cdbbdecbe28cce0
Arahma <bimaaurasakti2001@gmail.com>
2023-05-18 12:05:00 +0700

    create user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $users = [
+            [
+                'role_id' => 1,
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin2001'),
+            ],
+            [
+                'role_id' => 2,
+                'name' => 'member',
+                'email' => 'member@gmail.com',
+                'password' => Hash::make('member2001'),
+            ]
+        ];
+
+        User::insert($users);
+    }
+}
