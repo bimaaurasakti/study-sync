da6fa64f872fd1c9f04dbce869c987e2183ba256
Arahma <bimaaurasakti2001@gmail.com>
2023-05-21 23:30:01 +0700

    fix bug member page return error and add orderby on task list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,18 +13,22 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $inprogressTasks = Task::with(['activities'])->whereHas('activities', function($query) {
-            $query->where('user_id', auth()->user()->id)->where('activity_status_id', 2);
-        })->get();
-        $doneTasks = Task::with(['activities'])->whereHas('activities', function($query) {
-            $query->where('user_id', auth()->user()->id)->where('activity_status_id', 3);
-        })->get();
-
-        $inprogressTaskList = $inprogressTasks->pluck('id')->toArray();
-        $doneTaskList = $doneTasks->pluck('id')->toArray();
-        $notANewTasks = array_merge($inprogressTaskList, $doneTaskList);
-        $newTasks = Task::with(['activities'])->get()->whereNotIn('id', $notANewTasks);
+        $newTasks = $inprogressTasks = $doneTasks = null;
         $subjects = Subject::get(); 
+        
+        if (auth()->check()) {
+            $inprogressTasks = Task::with(['activities'])->whereHas('activities', function($query) {
+                $query->where('user_id', auth()->user()->id)->where('activity_status_id', 2);
+            })->orderBy('due_date')->get();
+            $doneTasks = Task::with(['activities'])->whereHas('activities', function($query) {
+                $query->where('user_id', auth()->user()->id)->where('activity_status_id', 3);
+            })->orderBy('due_date')->get();
+    
+            $inprogressTaskList = $inprogressTasks->pluck('id')->toArray();
+            $doneTaskList = $doneTasks->pluck('id')->toArray();
+            $notANewTasks = array_merge($inprogressTaskList, $doneTaskList);
+            $newTasks = Task::with(['activities'])->orderBy('due_date')->get()->whereNotIn('id', $notANewTasks);
+        }        
 
         return view('home', compact('subjects', 'newTasks', 'inprogressTasks', 'doneTasks'));
     }
